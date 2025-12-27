843b860ca0c090dbc745d226c3ab4214d511b515
VLTHellolin <i@hellolin.top>
2025-12-27 19:28:39 +0800

    feat!: major rewrite

--- eslint.config.js
+++
@@ -1,8 +0,0 @@
-import defineConfig from '@hellolin/eslint-config';
-
-export default defineConfig({
-  typescript: false,
-  yaml: false,
-  markdown: false,
-  json: true,
-});
--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  '**/*.{?([cm])[jt]s?(x),json?([c5]),y?(a)ml,md}': [
-    'eslint --cache --fix',
-  ],
-};
